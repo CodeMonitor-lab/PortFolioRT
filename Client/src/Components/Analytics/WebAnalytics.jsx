849a0d53495188f3d7ed18d071e3292c5d5dfba9
CodeMonitor-lab <niteshcse3333@gmail.com>
2025-05-09 22:16:31 +0300

    Add vercel modifeid file

---
+++ Client/src/Components/Analytics/WebAnalytics.jsx
@@ -0,0 +1,14 @@
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
